6975f80f28e9a1fd5fc56c07576378ed2d97457c
PauRomeroGH <paularomero.ct@gmail.com>
2026-05-30 15:16:19 -0300

    Extensión de CatalogoMaterias con nuevos métodos

--- src/modelo/CatalogoMaterias.java
+++ src/modelo/CatalogoMaterias.java
@@ -22,5 +22,17 @@ public class CatalogoMaterias extends Lista2DLinkedL{
 		
 		return ((Materia)elemento1).getCodigo() > ((Materia)elemento2).getCodigo();
 	}
+	
+	public int obtenerIndice(Materia materia) {
+		return buscar(materia);
+	}
+
+	public Materia obtenerMateria(int indice) {
+		return (Materia) devolver(indice);
+	}
+
+	public boolean existe(Materia materia) {
+		return buscar(materia) != -1;
+	}
 
 }
