ef2f145326e00531973649127c399536c96a15e0
Gabriel-515 <gabriel201744123@gmail.com>
2026-06-01 09:47:31 -0300

    cambios en GUI

--- src/GUI/BorderPaneAlumno.java
+++ src/GUI/BorderPaneAlumno.java
@@ -2,15 +2,24 @@ package GUI;
 
 
 
+
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import modelo.Alumno;
+import modelo.EstadoAcademico;
+import modelo.EstadoMateria;
+import modelo.HistorialAlumnos;
+import modelo.Materia;
 import modelo.PlanEstudio;
 
 public class BorderPaneAlumno extends BorderPane{
@@ -18,7 +27,11 @@ public class BorderPaneAlumno extends BorderPane{
 	private PlanEstudio plan;
 	private Alumno AlumnoActual;
 	private TextField txtBuscador;
-	private StackPane panelAlumno;
+	private StackPane panelAlumnoCenter;
+	private StackPane panelAlumnoTop;
+	
+	private HBox HBoxBuscador;
+	private BorderPane BorderPaneAlumno;
 	
 	
 	public BorderPaneAlumno(PlanEstudio plan) {
@@ -27,9 +40,13 @@ public class BorderPaneAlumno extends BorderPane{
 		this.AlumnoActual= null;
 		
 		
-		this.panelAlumno = new StackPane();
+		this.panelAlumnoCenter = new StackPane();
+		this.panelAlumnoTop = new StackPane();
 		
-		setCenter(panelAlumno);
+		setCenter(panelAlumnoCenter);
+		setTop(panelAlumnoTop);
+		this.setPadding(new Insets(20));
+		
 		
 		mostrar();
 		
@@ -38,18 +55,28 @@ public class BorderPaneAlumno extends BorderPane{
 	
 	public void mostrar() {
 		
-		panelAlumno.setStyle("-fx-background-color: #0c062e;");
+		panelAlumnoCenter.setStyle("-fx-background-color: #0c062e;");
+		panelAlumnoCenter.setPadding(new Insets(20));
+		
+		panelAlumnoTop.setStyle("-fx-background-color: #0c062e;");
 		
 		
-		HBox panelBuscador= new HBox();
-		crearPanelBuscador(panelBuscador);
+		
+		this.HBoxBuscador= new HBox();
+		crearPanelBuscador(this.HBoxBuscador);
 		
 		
-		panelAlumno.getChildren().add(panelBuscador);
+		panelAlumnoCenter.getChildren().add(this.HBoxBuscador);
 		
 		
 		
 	}
+	
+	
+	
+	
+	
+	
 	
 	
 	public void crearPanelBuscador(HBox panelHBox) {
@@ -84,12 +111,105 @@ public class BorderPaneAlumno extends BorderPane{
 		btnBus.setOnAction(evento ->{
 			String textoIngresado = this.txtBuscador.getText();
 			
+			try {
+				
+				int legajo= Integer.parseInt(textoIngresado);
+				this.AlumnoActual = plan.buscarAlumno(legajo);
+				
+				if(this.AlumnoActual != null) {
+					
+					this.panelAlumnoCenter.getChildren().clear();
+					this.panelAlumnoTop.getChildren().clear();
+					
+					this.panelAlumnoTop.getChildren().add(this.HBoxBuscador);
+					
+					createBorderPaneAlumno();
+					
+				}
+				
+			}catch (NumberFormatException e) {
+				System.out.println(e);
+			}
+			
 		} );
 	}
 	public void creadorTxtBuscador(TextField txtbus) {
 		txtbus.setPrefHeight(Double.MAX_VALUE);
 		txtbus.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
 		txtbus.setPromptText("ingresar nuemro de legajo");
+		
+		
+	}
+	
+	// crear el panel que contiene a los alumnos 
+	public void createBorderPaneAlumno() {
+		BorderPane alumnoInfo = new BorderPane();
+		
+		alumnoInfo.setMaxWidth(600);
+		alumnoInfo.setMaxHeight(Double.MAX_VALUE);
+		alumnoInfo.setStyle("-fx-background-color: #190d5d;");
+		
+		if(this.AlumnoActual !=null) {
+			
+			Label numLegajo = new Label();	numLegajo.setStyle("-fx-text-fill: white;");
+			Label nombreAlumno = new Label();nombreAlumno.setStyle("-fx-text-fill: white;");
+			
+			numLegajo.setText("numero de legajo: "+this.AlumnoActual.getLegajo()); 
+			nombreAlumno.setText("nombre: "+this.AlumnoActual.getNombre());
+			
+			HBox HBoxnombre = new HBox();
+			HBoxnombre.setStyle("-fx-background-color: #190d5d;");
+			HBoxnombre.setPadding(new Insets(5));
+			HBoxnombre.setSpacing(30);
+			HBoxnombre.setAlignment(Pos.CENTER_LEFT);
+			HBoxnombre.getChildren().addAll(numLegajo,nombreAlumno);
+			
+			alumnoInfo.setTop(HBoxnombre);
+			
+			
+			ScrollPane paneContenedorHisortial = new ScrollPane();
+			paneContenedorHisortial.setStyle(
+		            "-fx-background: #190d5d; " +          
+		            "-fx-background-color: #190d5d; " +    
+		            "-fx-border-color: #190d5d;"           
+		        );
+			paneContenedorHisortial.setFitToWidth(true);
+			paneContenedorHisortial.setMaxHeight(Double.MAX_VALUE);
+			
+	        
+	        
+	        
+			VBox VBoxHistorail = new VBox();
+			
+			paneContenedorHisortial.setContent(VBoxHistorail);
+			VBoxHistorail.setStyle("-fx-background-color: #190d5d;");
+			
+			
+			
+			HistorialAlumnos historial= this.AlumnoActual.getHistorial();
+			for(int i=0; i < historial.tamanio(); i++) {
+				
+				EstadoMateria estadoMateria =(EstadoMateria) historial.devolver(i);
+				Materia materia =(Materia) this.plan.getCatalogoMaterias().getMateria(estadoMateria.getCodigoMateria() ) ;
+				Label lblId = new Label();		lblId.setText(materia.getCodigo()+"");	lblId.setMinWidth(30);	lblId.setStyle("-fx-text-fill: white;");
+				Label lblNombre = new Label();	lblNombre.setText(materia.getNombre()); lblNombre.setMaxWidth(Double.MAX_VALUE);	lblNombre.setStyle("-fx-text-fill: white;");
+				Label lblEstado	= new Label();	lblEstado.setText(estadoMateria.getEstado().name()); lblEstado.setMinWidth(100);	lblEstado.setStyle("-fx-text-fill: white;");
+				
+				HBox borderPaneMateria = new HBox();
+				
+				borderPaneMateria.setPadding( new Insets(10));
+				if(estadoMateria.getEstado() != EstadoAcademico.NO_CURSADA) {
+					borderPaneMateria.getChildren().addAll(lblId, lblNombre,lblEstado);
+					VBoxHistorail.getChildren().add(borderPaneMateria);
+				}
+				
+				VBoxHistorail.setAlignment(Pos.TOP_CENTER);
+				HBox.setHgrow(lblNombre, Priority.ALWAYS);
+			}
+			
+			alumnoInfo.setCenter(paneContenedorHisortial);
+			this.panelAlumnoCenter.getChildren().add(alumnoInfo);
+		}
 		
 		
 	}
