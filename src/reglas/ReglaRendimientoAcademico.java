e4fbe176b71e6aee2694ff75158a22384c0eb3a8
Gabriela Salas <salasgabriela232@gmail.com>
2026-06-18 00:20:04 -0300

    Se eliminaron algunos comentarios innecesarios

--- src/reglas/ReglaRendimientoAcademico.java
+++ src/reglas/ReglaRendimientoAcademico.java
@@ -36,10 +36,9 @@ public class ReglaRendimientoAcademico implements ReglaAcademica{
 	public ResultadoRegla evaluar(SolicitudCondicional solic, PlanEstudio plan) {
 	
 		// 1. Calculamos el total de cuatrimestres de la carrera
-		// (Ej: TUP de 3 años = 6 cuatrimestres. LAS de 5 años = 10 cuatrimestres)
 		int totalCuatrimestresCarrera = plan.getCatalogoMaterias().getMateria(plan.getCatalogoMaterias().tamanio()).getAnio()* 2;
 		    
-		// 2. Aplicamos tu idea: Cortamos la carrera a la mitad exacta
+		// 2. Cortamos la carrera a la mitad exacta
 		int cuatrimestreMitad = totalCuatrimestresCarrera / 2;
 		    
 		// 3. Calculamos en qué cuatrimestre neto está la materia que piden
@@ -47,13 +46,13 @@ public class ReglaRendimientoAcademico implements ReglaAcademica{
 		// Ej: 2° año, 2° cuatri -> (2-1)*2 + 2 = Cuatrimestre 4
 		int cuatrimestreMateriaNeto = (solic.getMateriaSolicitada().getAnio() - 1) * 2 + solic.getMateriaSolicitada().getCuatrimestre();
 		    
-		// 🔍 CONTROL INTELIGENTE BASADO EN TU PROPUESTA:
+		
 		// Si la materia está en la primera mitad de la carrera, queda exenta del 60%
 		if (cuatrimestreMateriaNeto <= cuatrimestreMitad) {
 		   	return new ResultadoRegla(true, "Exento: El requisito del 60% aprobado no aplica para materias de la primera mitad de la carrera.");
 		}
 		    
-		    // 📐 Si pasó la mitad de la carrera, recién ahí exigimos el 60% real:
+		    //Si pasó la mitad de la carrera, recién ahí exigimos el 60% real:
 		int totalMaterias = plan.getCatalogoMaterias().tamanio();
 		int aprobadas = solic.getAlumno().getHistorial().getContadorAprobadas();
 		double porcentaje = ((double) aprobadas / totalMaterias) * 100;
